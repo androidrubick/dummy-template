9fcf2bfd5926912c78065a87eb1f808776ab8877
yong01.yin <yong01.yin@vipshop.com>
2017-11-23 11:23:01 +0800

    fix some gradle script error

---
+++ app/src/main/java/pub/androidrubick/demo/MainActivity.java
@@ -0,0 +1,23 @@
+package pub.androidrubick.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.vip.makeup.sdk.resource.demo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv = (TextView) findViewById(R.id.tv);
+    }
+}
